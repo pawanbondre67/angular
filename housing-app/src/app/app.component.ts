ff952e2001df2867d22686f08f7fb6140ea6be96
pawanbondre67 <bondrepavan623@gmail.com>
2024-03-09 23:35:42 +0530

    initial commit

--- housing-app/src/app/app.component.ts
+++ housing-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet , HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'housing-app';
+title = 'House';
 }
